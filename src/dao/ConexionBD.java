df01f70bb2bd941d3ff0bd21088067aeb625ee8b
jaine <jaine@JASA>
2025-08-12 14:20:33 -0600

    Sexto commit "  agregue los importes para que funcione el codigo de la conexion a la base de datos"

--- src/dao/ConexionBD.java
+++ src/dao/ConexionBD.java
@@ -3,11 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
  * @author jainer Said Garcia Gonzalez
  */
+
 public class ConexionBD {
     
 }
