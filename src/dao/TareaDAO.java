ff05d441b7256a6690a5aa9ab1787ec44b2b23aa
jaine <jaine@JASA>
2025-08-12 15:00:01 -0600

    Noveno commit "Agregue parte del codigo para la clase TareaDAO "

--- src/dao/TareaDAO.java
+++ src/dao/TareaDAO.java
@@ -3,11 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
+import dominio.Tarea;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  *
- * @author jaine
+ * @author jainer Said Garcia Gonzalez
  */
+
 public class TareaDAO {
-    
+
+    public void insertarTarea(Tarea tarea) throws SQLException {
+        String sql = "INSERT INTO Tarea (titulo, prioridad, estado, especial, fecha) VALUES (?, ?, ?, ?, ?)";
+
+        try (Connection conn = ConexionBD.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, tarea.getTitulo());
+            stmt.setInt(2, tarea.getPrioridad());
+            stmt.setBoolean(3, tarea.isEstado());
+            stmt.setBoolean(4, tarea.isEspecial());
+            stmt.setObject(5, tarea.getFecha()); 
+
+            stmt.executeUpdate();
+        }
+    }
 }
