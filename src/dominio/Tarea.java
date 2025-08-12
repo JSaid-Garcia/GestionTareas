b2869a11bb5fd0ca5ddc86d9c33a2523de2722d1
jaine <jaine@JASA>
2025-08-12 14:09:36 -0600

    Cuarto commit "  agregue sus el constructor y los setters y getters a la clase Tarea"

--- src/dominio/Tarea.java
+++ src/dominio/Tarea.java
@@ -20,6 +20,89 @@ public class Tarea {
     private Date fecha;
     private LocalDate creado;
     private LocalDate modificado;
+
+    //constructor
+    public Tarea(int idTarea, String titulo, int prioridad, boolean estado, boolean especial, Date fecha, LocalDate creado, LocalDate modificado) {
+        this.idTarea = idTarea;
+        this.titulo = titulo;
+        this.prioridad = prioridad;
+        this.estado = estado;
+        this.especial = especial;
+        this.fecha = fecha;
+        this.creado = creado;
+        this.modificado = modificado;
+    }
+
+    
+    
+    //Setters y getters
+    
+    public void setIdTarea(int idTarea) {
+        this.idTarea = idTarea;
+    }
+    
+    public int getIdTarea() {
+        return idTarea;
+    }
+
+   public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEspecial(boolean especial) {
+        this.especial = especial;
+    }
+
+    public boolean isEspecial() {
+        return especial;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setCreado(LocalDate creado) {
+        this.creado = creado;
+    }
+
+    public LocalDate getCreado() {
+        return creado;
+    }
+
+   
+    public void setModificado(LocalDate modificado) {
+        this.modificado = modificado;
+    }
+
+    public LocalDate getModificado() {
+        return modificado;
+    }
+
     
    
     
