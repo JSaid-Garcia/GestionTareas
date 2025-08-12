e21d8c197a7e9ef1eeeacb864ea41187baf57b87
jaine <jaine@JASA>
2025-08-12 13:59:08 -0600

    Segundo commit " Le agregue sus atributos privados a la clase Tarea"

--- src/dominio/Tarea.java
+++ src/dominio/Tarea.java
@@ -3,11 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dominio;
+import java.time.LocalDate;
+import java.util.Date;
 
 /**
  *
  * @author jainer Said Garcia Gonzalez
  */
 public class Tarea {
+    //Atributos
+    private int idTarea;
+    private String titulo;
+    private int prioridad;
+    private boolean estado;
+    private boolean especial;
+    private Date fecha;
+    private LocalDate creado;
+    private LocalDate modificado;
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
